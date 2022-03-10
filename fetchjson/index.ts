0fc5e1fe6aae06cc213061876635bfcfc519f12c
Shiyu <shiyu.huang93@gmail.com>
2022-03-10 15:55:03 +0800

    used ts to catch simple errors

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,9 +2,29 @@ import axios from 'axios';
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  console.log(response.data);
-})
+  const todo = response.data as Todo;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
+});
+
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The TODO with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
+}
 
 // to compile: tsc index.ts
 // to run: node index.js
